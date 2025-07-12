496f998f07766badbc1987d14a7970af477812eb
Daniel88V <daniel12velo@gmail.com>
2025-07-11 21:06:31 -0600

    Actividad5

--- Listado_Estudiantes.py
+++ Listado_Estudiantes.py
@@ -1,2 +1,17 @@
+class Estidiante:
+    def __init__(self, nombre, carne, carrera, nota_final):
+        self.nombre = nombre
+        self.carne = carne
+        self.carrera = carrera
+        self.nota_final = nota_final
+
 def main():
     print(" ===BIENVENIDO AL MENÚ===")
+    print("Seleccione una opción")
+    print("1. Registrar nuevo estudiante")
+    print("2. Mostrar listado de estudiantes")
+    print("3. Buscar estudiante (por carné)")
+    print("4. Calcular promedio de todos los estudiantes")
+    print("5. Salir")
+if __name__ == "__main__":
+    main()
