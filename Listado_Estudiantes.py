dde09d0fd5705193bbfc8ed19a4a448a5799011f
Daniel88V <daniel12velo@gmail.com>
2025-07-11 21:13:48 -0600

    Actividad5

--- Listado_Estudiantes.py
+++ Listado_Estudiantes.py
@@ -4,6 +4,15 @@ class Estidiante:
         self.carne = carne
         self.carrera = carrera
         self.nota_final = nota_final
+        self.estudiante = []
+    def ingreso_estudiante(self, estudiante):
+        self.estudiante.append(estudiante)
+        print(f"Estudiante {self.nombre} ingresado correctamente")
+    def __str__(self):
+        return f"Alumno: {self.nombre}| Carné: {self.carne}| Carrera: {self.carrera}| Nota final: {self.nota_final}"
+class Clase:
+    def __init__(self, grado):
+        self.nombre = grado
 
 def main():
     print(" ===BIENVENIDO AL MENÚ===")
